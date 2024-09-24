e2957195727f15ceba8be3845f292488cd093bfe
kio@kiochan.one <xingchen.sun@rhenag.de>
2024-09-24 17:43:27 +0200

    refactor(next-ui-portal): add build process and remove js helper scripts

--- app/web/tailwind.config.js
+++ app/web/tailwind.config.js
@@ -1,6 +1,52 @@
 // @ts-check
-// TODO: "require('@kiochan/next-ui-portal')" didn't work
-const { generateTailwindConfig } = require('../../lib/next-ui-portal');
+// import type { Config } from 'tailwindcss/types/config';
+/**
+ * @typedef {import('tailwindcss/types/config').Config} Config
+ */
+const { join } = require('path');
 
-/** @type {import('tailwindcss').Config} */
+const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
+const { nextui } = require('@nextui-org/react');
+
+const { nextuiConfig } = require('@kiochan/next-ui-portal');
+
+/**
+ * Generates a Tailwind CSS configuration object based on the provided directory.
+ * This function dynamically sets the paths and other configurations needed for Tailwind to function properly in a given project structure.
+ *
+ * @param {string} dirname - The directory name where the Tailwind configuration will be applied. This is typically the root of your project.
+ * @returns {Config} The Tailwind CSS configuration object customized for the provided directory.
+ */
+function generateTailwindConfig(dirname) {
+  const workspaceRoot = join(dirname, '../../');
+  const projectRoot = dirname;
+
+  return {
+    content: [
+      // next.js app
+      ...createGlobPatternsForDependencies(projectRoot),
+      join(
+        projectRoot,
+        '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'
+      ),
+      // lib/*
+      join(
+        workspaceRoot,
+        'lib/*/',
+        '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'
+      ),
+      // next-ui preset
+      join(
+        workspaceRoot,
+        './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
+      ),
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [nextui(nextuiConfig)],
+    darkMode: 'class',
+  };
+}
+
 module.exports = generateTailwindConfig(__dirname);
--- lib/next-ui-portal/README.md
+++ lib/next-ui-portal/README.md
@@ -1,7 +1,7 @@
-# next-ui-portal
+# NextUi Portal
 
 This library was generated with [Nx](https://nx.dev).
 
 ## Running unit tests
 
-Run `nx test next-ui-portal` to execute the unit tests via [Jest](https://jestjs.io).
+Run `nx test @kiochan/next-ui-portal` to execute the unit tests via [Vitest](https://vitest.dev/).
