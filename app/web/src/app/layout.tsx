5ce0bfcd50703dde56a732c3b3c8b2b63b8d7485
kiochan <kio@kiochan.one>
2024-10-05 05:47:28 +0200

    style: use eslint fix

--- app/web/src/app/layout.tsx
+++ app/web/src/app/layout.tsx
@@ -1,13 +1,15 @@
-import { Viewport } from 'next/types'
+import type { Viewport } from 'next/types'
 import { globalConfig } from '../defs/global-config'
 import './global.css'
 
 import { Providers } from '../helper/providers'
 import { getBodyClassName } from '@kiochan/next-ui-portal'
 
+const initialScale = 1
+
 export const viewport: Viewport = {
   width: 'device-width',
-  initialScale: 1,
+  initialScale,
   viewportFit: 'cover',
 }
 
@@ -15,7 +17,7 @@ export interface RootLayoutProps {
   children: React.ReactNode
 }
 
-export default function RootLayout(props: RootLayoutProps) {
+export default function RootLayout(props: RootLayoutProps): React.JSX.Element {
   return (
     <html lang={globalConfig.site.language}>
       <body className={getBodyClassName(globalConfig)}>
